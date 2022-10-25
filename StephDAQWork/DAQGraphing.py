d11bb93ddcf85442ca10f1ef154659a572e6bb0e
Stephanie Howell <102824003+Stepo14@users.noreply.github.com>
2022-10-25 13:39:41 -0600

    Add files via upload

---
+++ StephDAQWork/DAQGraphing.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 12:02:56 2022
+
+@author: Steph
+"""
+
+# Basic line graph
+
+# All this graphing should automatically be done in a separate window
+# according to the settings I have picked
+
+# Begin on graph 1
+# Take flux values and graph voltage values
+# Increment graphs as bias increases
+# Some sort of while statement that plots the data for a specific bias value
+# Read data from Kalamari Code for the specific bias value
+# Increment the axs[] as the bias increases
+
+# Create 4 graphs
+# Constrained layout
+# Automatically adjusts things so they cleanly fit in the figure window
+# Needs to be activated before any axes are added
+# It looks like you actually plot the graph using a for loop and increment through
+# axs[i]
+
+# Superimpose all the graphs on each other
+
+# Boom, done, jupyter script replica
