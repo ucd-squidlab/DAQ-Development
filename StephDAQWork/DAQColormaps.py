cff3d6277f9664f0c415e9a760177eeadd96e136
Stephanie Howell <102824003+Stepo14@users.noreply.github.com>
2023-02-02 13:52:58 -0700

    Update DAQColormaps.py

--- StephDAQWork/DAQColormaps.py
+++ StephDAQWork/DAQColormaps.py
@@ -16,8 +16,9 @@ Created on Mon Jan 30 13:59:58 2023
 # pyplot functions as plt
 
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-import matplotlib as mlp
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 """ I'm not sure yet as to why these were imported """
 
@@ -33,3 +34,35 @@ import matplotlib.pyplot as plt
 # The last key part of this code is that it needs to incorporate some sort of
 # crosshair or dial that can scan along the x and y axes of each plot
 # simultaneously and read the 'color value' off each plot
+
+
+""" 
+The following code is 'dummy code' I used to test the custom cmap capabilities 
+"""
+
+# Randomly generate x and y values
+
+x = np.random.random(200)
+y = np.random.random(200)
+
+# This part was retrieved from a youtube tutorial I had watched
+# I'm not necessarily sure why he calls it 'classes' because that makes me
+# associate it with python class(es)
+# I'm not sure what randint means
+
+classes = np.random.randint(0, 100, 200)
+
+# Create a custom color map for all intents and purposes
+# LinearSegmentedColormap will give us a spectrum of colors
+# "custom" is an arbitrary name I chose for a required input
+# Some colors are listed by names others are listed by hex code because they
+# do not have a name recognized by matplotlib
+# This spectrum should mimic the colorbars in the other SOT journals
+
+custom_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+    "custom", ["#00008B", "blue", "cyan", "green", "yellow", "orange", "red", "#8B0000"])
+
+# When plotting
+plt.scatter(x, y, c=classes, cmap=custom_cmap)
+plt.colorbar()
+plt.show()
