9a9c88d1a9d070ea03fdb13ff945ce986dd077cb
SQUID Lab Intern <slintern@SQUIDs-Mac-mini.local>
2023-04-26 17:34:09 -0600

    Removed getmac and finished read, write, and ask

--- prologix/plgxgpib.py
+++ prologix/plgxgpib.py
@@ -13,8 +13,10 @@ This program is free software licensed under the apache license
 """
 
 import socket
-from scapy.all import getmacbyip
+#import pandas as pd
+#import subprocess
 import regex as re
+from time import sleep
 
 
 
@@ -32,11 +34,13 @@ class prologixEthernet:
 
         ipaddr:str IP Address of Prologix Controller
         port:str default="1234" Port to use to connect to prologix controller
-        timeout:float default=5 Timeout to wait for command
+        timeout:float default=5 Timeout to wait for return from controller
         """
-        #Checks to ensure we are looking at the coorrect object
-        """
-        self.MAC = self.isGpib(ipaddr)
+        # Checks to ensure we are looking at the coorrect object
+
+        # Update: This code won't work because there is no easy way to find the mac addresses of  
+
+        # self.MAC = self.isGpib(ipaddr)
 
         # Creates the web socket, uses IPV4 
         self.plgx = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -46,7 +50,6 @@ class prologixEthernet:
         address = (ipaddr, port)
         # Open the socket!
         self.plgx.connect(address)
-"""
 
 
 
@@ -57,7 +60,7 @@ class prologixEthernet:
         addr:str Address to query
         """
         try:    # Gets the mac address associated with the IP address
-            mac = getmacbyip(addr)
+            mac = self.getmac(addr)
         except:  # If it doesn't connect returns error
             raise ValueError("Incorrect IP Address")
         else: # if it doesn't error out above this compares the mac addresses
@@ -68,36 +71,70 @@ class prologixEthernet:
             # evaluates to false which means that the device was made by prologix and so returns the mac address
             if not (self.PLGX_MAC - (mac >> 24)): return mac
             else: raise ValueError(f"Device at {addr} not Prologix device!")
+    """
+    def getmac(self, IP:str, subnet:bool = False):
+        """
+    """
+        Function which finds the mac address of a given IP address or subnet
 
+        If subnet is selected then it will return a list of all mac and IP addresses
+        on the subnet. 
+
+        IP:str IP address to find mac for. If subnet=True, IP should be of form "192.168.1.x"
+        subnet:bool Default:False; if set to true the function will list all MAC and IP addresses on the subnet 
+        """ """
+        subprocess.run(["ping", "-c", "2", 255.255.255.255])
+        arps = pd.DataFrame(
+            [a.split(" ") for a in subprocess.run(['arp', '-a'],
+                                                stdout=subprocess.PIPE).stdout.decode().split("\n")[0:-2]],
+            columns=["host","ip","a1","mac","a5","a6","a7","a8", "a9"])
+        iplist = arps.ip.to_list()
+        maclist = arps.mac.to_list()
+        for i, k in enumerate(iplist):
+            iplist[i] = re.sub("[\(\)]", "", k)
+        print(iplist)
+        try:
+            mac = maclist[iplist.index(IP)]
+        except:
+            raise ValueError(f"IP Address {IP} not found on subnet {ip}")
+        else:
+            return mac
+"""
     @property
     def address(self):
-        pass
-        self.addr
+        """
+        Creates a property which is the GPIB address
+        """
+        self.addr = int(self.ask("++addr"))
+
+    @address.setter
+    def address(self, new_addr):
+        """
+        Sets the GPIB address of the bus
+        """
+        self.addr = new_addr
+        self.write(f"++addr {new_addr}")
 
     @property
     def readafterwrite(self):
+        """
+        Should the controller have the instrument talk immediately after reading?
+        """
         self._auto = bool(int(self.ask("++auto")))
         return self._auto
 
     @readafterwrite.setter
-    def readafterwrite(self, RAW:bool):
+    def readafterwrite(self, RAW):
         self._auto = bool(RAW)
         self.write(f"++auto {int(self._auto)}", escape=False)
 
 
     def write(self, command:str, auto:bool=False, escape=True):
-        """
-        Sends command to instrument and does not read response
 
-        command:str Command to send to instrument
-        auto:bool False If True this will direct instrument to talk after command is written
-        escape:bool True if command contains [+ \\n \\r] characters this escapes them with \\x1B character so they will be passed to instrument and not captured by Prologix
-        """
         # Make it into an ascii string stored as a bytearray
         command = command.encode(encoding='ascii')
         #If it is set to escape special characters then this will do it, used for 
         # + \n and \r and escapes them using the ascii character \x1B. 
-
         #print(command.hex())
         #print(bool(re.search(b"[\x0A\x0D\x2b]", command)))
         #print(re.search(b"[\x0A\x0D\x2b]", command))
@@ -106,28 +143,17 @@ class prologixEthernet:
             command = re.sub(rb"(\x0A)", rb"\x1B\1", command)
             command = re.sub(rb"(\x0D)", rb"\x1B\1", command)
             command = re.sub(rb"(\x2B)", rb"\x1B\1", command)
-
-        # Appends ethernet termination character to command
-        command = command+b"\n"
-
-        if auto:
-            self.plgx.send(b"++auto 1\n")
-        else:
-            self.plgx.send(b"++auto 0\n")
-
         #print(command.hex())
-        # Sends command to instrument and stores number of bytes sent 
-        bts = self.plgx.send(command)
+        command = command + b'\x0A'
+        self.plgx.send(command)
 
-        # Returns zero if it transmitted all bites 
-        return bts-len(command)
-
-
-
-    def read(self):
-        pass
-    def ask(self, command):
-        pass
+    def readall(self):
+        return self.plgx.recv(8192).rstrip()
+    
+    def ask(self, command:str, wait:float, Escape:bool=True):
+        self.write(command, escape=Escape)
+        sleep(wait)
+        return self.readall()
         
 
 
@@ -146,7 +172,7 @@ class prologixEthernet:
 
 class Instrument():
 
-    def __init__(self, controller:object, addr, terminator:bytes = EOI):
+    def __init__(self, controller:prologixEthernet, addr:int, auto:bool=True, terminator:bytes = EOI):
         """
         Instrument object 
         addr:int Address of GPIB device to access
@@ -159,7 +185,11 @@ class Instrument():
         self.address = addr
         #GPIB Identifier
         self.IDN = 3
+        self.auto = auto
     
     def _ControlBus(self):
-        pass
-
+        if self.bus._auto != self.auto:
+            self.bus.readafterwrite = self.auto
+        
+        if self.bus.addr != self.address:
+            self.bus.address = self.address
